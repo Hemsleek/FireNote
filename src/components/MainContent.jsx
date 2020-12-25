de9b0c61aacd796d107c23e79f678e65a5df3f32
hemsleek <asiyanbimubashir@gmail.com>
2020-12-25 23:08:40 +0100

    feat: Nav icons

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const MainContent = () => {
+    const actions = 'clock,download-icon,delete,info'.split(',')
     return (
-        <div>
-            main content
+        <div className="MainContent">
+            <nav className="flex items-center ">
+                <img className="w-6 h-6" src="/assets/slider.svg" alt="menu"/>
+                <span className='flex-grow'/>
+                {
+                    actions.map((action , actionIndex) => ( <img key={`nav_action${actionIndex}`} className="w-6 h-6" src={`/assets/${action}.svg`} alt={action} />))
+                }
+               
+
+            </nav>
         </div>
     )
 }
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const SideNav = () => {
     return (
         <div className="SideNav">
-            <nav className="flex justify-between min-w-32">
-                <img className="w-4 h-4" src="/assets/menu.svg" alt="menu"/>
-                <img className="w-4 h-4" src="/assets/new-note.svg" alt="new-note"/>
+            <nav className="flex justify-between ">
+                <img className="w-6 h-6" src="/assets/menu.svg" alt="menu"/>
+                <img className="w-6 h-6" src="/assets/new-note.svg" alt="new-note"/>
 
             </nav>
         </div>
